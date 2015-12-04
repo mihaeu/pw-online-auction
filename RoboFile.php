fca03fb66f5b2310a3fd5a8d8b02e4a352bf63b5
Michael Haeuslmann <haeuslmann@gmail.com>
2015-12-04 13:27:59 +0100

    Bootstrap project

---
+++ RoboFile.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This is project's console commands configuration for Robo task runner.
+ *
+ * @see http://robo.li/
+ */
+class RoboFile extends \Robo\Tasks
+{
+    public function test()
+    {
+        $this->taskPhpUnit('./phpunit.phar')
+            ->configFile('phpunit.xml.dist')
+            ->run();
+    }
+
+    public function phpab()
+    {
+        $this->_exec('php phpab.phar -o src/autoload.php src');
+    }
+
+    public function dev()
+    {
+        $this->taskWatch()
+            ->monitor('src', function () {
+                $this->phpab();
+            })
+            ->run();
+    }
+}
---
+++ tests/Test.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types = 1);
+
+class Test extends PHPUnit_Framework_TestCase
+{
+    public function testTest()
+    {
+        $this->assertTrue(true);
+    }
+}
